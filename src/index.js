83d40bf84fe3c31823c3e2b5b79b7b145fb6307e
Jason Hirata <abstractalchemist@gmail.com>
2017-08-26 16:21:04 -1000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Words from './words'
+
+function Main(words) {
+    return (<div />)
+}
+
+document.addEventListener('DOMContentLoaded', _ => {
+    ReactDOM.render(<Main/>, document.querySelector('#content');
+})
